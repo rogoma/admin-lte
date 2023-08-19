0c7cde984ec756aa81fdde013a28d6a8dba19fd3
rogoma <rogoma007@gmail.com>
2023-08-19 19:58:09 -0400

    Proyecto con LTE3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">HOME</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">CONTENIDO!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
